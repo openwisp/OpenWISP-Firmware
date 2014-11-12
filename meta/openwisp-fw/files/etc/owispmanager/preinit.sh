6611f12a950801bedf9e86331d5165062bda2a98
cvalenti <c.valenti@unidata.it>
2014-11-12 15:43:23 +0100

    Add preinit script that move/remove files and configure hostname and timezone

---
+++ meta/openwisp-fw/files/etc/owispmanager/preinit.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+FIREWALL_CONFIG_PATH="/etc/config/firewall"
+SYSTEM_CONFIG_PATH="/etc/config/system"
+
+## Disabling unneedeed services
+
+# firewall
+mkdir /etc/modules.d/disabled/
+mv $ROOTFS/etc/modules.d/*-ipt-conntrack /etc/modules.d/*-ipt-nat $ROOTFS/etc/modules.d/disabled/
+
+if [ -z "`grep mesh $FIREWALL_CONFIG_PATH`"]; then
+  rm /etc/rc.d/S*firewall 2>/dev/null
+fi
+
+# htpdate
+rm /etc/rc.d/S*htpdate 2>/dev/null
+
+# ntpdate
+rm /etc/rc.d/S*ntpdate 2>/dev/null
+
+# httpd
+rm /etc/rc.d/S*httpd 2>/dev/null
+
+# uhttpd
+rm /etc/rc.d/S*uhttpd 2>/dev/null
+
+# dnsmasq
+rm /etc/rc.d/S*dnsmasq 2>/dev/null
+
+## Set hostname
+sed -i 's/option[ \t]hostname[ \t].*$/option hostname Unconfigured/' $SYSTEM_CONFIG_PATH
+
+## Set timezone
+sed -i 's/option[ \t]timezone[ \t].*$/option timezone \"CET-1CEST-2,M3\.5\.0\/02:00:00,M10\.5\.0\/03:00:00\"/' $SYSTEM_CONFIG_PATH
